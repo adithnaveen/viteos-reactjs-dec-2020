ee37b3d18b072434dea264356723403f7718d69d
Naveen Kumar Srinivas <adith.naveen@gmail.com>
2020-12-21 13:00:30 +0530

    added controlled component

--- workspace/react-works-01/reactjs-demo01/src/components/ProductFormCtrl.js
+++ workspace/react-works-01/reactjs-demo01/src/components/ProductFormCtrl.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 
-class ProductForm extends Component {
+class ProductFormCtrl extends Component {
 
     state ={
         pname:"", 
@@ -13,12 +13,31 @@ class ProductForm extends Component {
             price : "Product Price Required", 
             desc:"Product Description Required", 
             make:"Product Make Required"
-        }
+        }, 
+        errorMessages:null
+    }
+
+    // helder function 
+    validateForm = (formErrors) => {
+        let valid = true; 
+        Object.values(formErrors).forEach(err => valid = valid && err.length ===0)
+        return valid; 
     }
 
     addNewProduct=(event)  => {
         console.log("Product Submitted.. ");
         event.preventDefault(); 
+
+        // check if there are any errors 
+        let {formErrors} = this.state; 
+
+        if(this.validateForm(formErrors)) {
+            alert("Form Submitted"); 
+        }else {
+            let errorMessages = Object.values(formErrors).map((err, index) => err.length===0 ?null :
+                 <li key={index}>{err}</li>)
+            this.setState({errorMessages})
+        }
     }
 
     tfHander =(evt) =>  {
@@ -50,8 +69,26 @@ class ProductForm extends Component {
                 }
                 break; 
             case 'desc': 
+            if(!value || value.length ===0) {
+                formErrors.desc="Product Description Required"
+            }
+            else if(value.length < 3 || value.length > 20) {
+                formErrors.desc = "Product description must be between 3 to 20 chars "
+            }
+            else {
+                formErrors.desc =''; 
+            }
                 break; 
             case 'make': 
+            if(!value || value.length ===0) {
+                formErrors.make="Product Make Required"
+            }
+            else if(value.length < 3 || value.length > 20) {
+                formErrors.make = "Product Make must be between 3 to 20 chars "
+            }
+            else {
+                formErrors.make =''; 
+            }
                 break; 
 
             default: 
@@ -72,6 +109,8 @@ class ProductForm extends Component {
 
                     <h3>Product Entry Form</h3>
                 <form className="form" onSubmit={this.addNewProduct}>
+
+
                     <div className="form-group row">
                         <label className="control-label col-md-4">Product Name</label>
                         <div className="col-md-8">
@@ -83,6 +122,7 @@ class ProductForm extends Component {
                                  className="form-control" />
                         </div>
                     </div>
+                    
 
                     <div className="form-group row">
                         <label className="control-label col-md-4">Product Price</label>
@@ -115,7 +155,10 @@ class ProductForm extends Component {
 
 
                     <button className="btn btn-primary">Save Product</button>
-
+                    <hr /> 
+                    <ul> 
+                    {this.state.errorMessages}
+                    </ul>
                 </form>
 
 
@@ -131,4 +174,4 @@ class ProductForm extends Component {
     }
 }
 
-export default ProductForm;
+export default ProductFormCtrl;
